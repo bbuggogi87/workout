782d0abb15928ae0dd785dd4a82910f266d8d444
bbuggogi87 <bbuggogi@gmail.com>
2026-06-27 07:07:52 +0900

    Refactor state initialization and applyCustomSuppsToDB

--- store.js
+++ store.js
@@ -3,29 +3,16 @@ import { FOOD_DB, FOOD_CATEGORIES, INITIAL_USER_INFO, INITIAL_CUSTOM_SUPPS, INIT
 export const state = {
     userInfo: JSON.parse(JSON.stringify(INITIAL_USER_INFO)),
     phases: JSON.parse(JSON.stringify(INITIAL_PHASES)),
-    currentPhaseId: 'p_1',
-    clipboardMeals: null,
-    editingMealState: null,
-    editingPhaseIsNew: false,
-    customSupps: JSON.parse(JSON.stringify(INITIAL_CUSTOM_SUPPS)),
-    foodDB: JSON.parse(JSON.stringify(FOOD_DB)),
-    foodCategories: JSON.parse(JSON.stringify(FOOD_CATEGORIES)),
-    pieChartInstance: null,
-    userId: null,
-    db: null,
-    appId: 'prep-master-pro'
+    currentPhaseId: 'p_1', clipboardMeals: null, editingMealState: null, editingPhaseIsNew: false,
+    customSupps: JSON.parse(JSON.stringify(INITIAL_CUSTOM_SUPPS)), foodDB: JSON.parse(JSON.stringify(FOOD_DB)), foodCategories: JSON.parse(JSON.stringify(FOOD_CATEGORIES)),
+    pieChartInstance: null, userId: null, db: null, appId: 'prep-master-pro'
 };
 
 export function applyCustomSuppsToDB() {
     state.foodCategories['보충제'] = [];
     state.customSupps.forEach(supp => {
         if(supp.weight > 0) {
-            state.foodDB[supp.name] = { 
-                c: supp.carbs / supp.weight, 
-                p: supp.protein / supp.weight, 
-                f: supp.fat / supp.weight, 
-                k: supp.kcal / supp.weight 
-            };
+            state.foodDB[supp.name] = { c: supp.carbs / supp.weight, p: supp.protein / supp.weight, f: supp.fat / supp.weight, k: supp.kcal / supp.weight };
             state.foodCategories['보충제'].push(supp.name);
         }
     });
