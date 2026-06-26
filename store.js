87ac467e4c9e7ac97334ab6e49e369eb06adbdb7
bbuggogi87 <bbuggogi@gmail.com>
2026-06-27 03:56:05 +0900

    Refactor state management to use window.PrepStore

--- store.js
+++ store.js
@@ -1,26 +1,31 @@
-import { FOOD_DB, FOOD_CATEGORIES, INITIAL_USER_INFO, INITIAL_CUSTOM_SUPPS, INITIAL_PHASES } from './constants.js';
-
-export const state = {
-    userInfo: JSON.parse(JSON.stringify(INITIAL_USER_INFO)),
-    phases: JSON.parse(JSON.stringify(INITIAL_PHASES)),
-    currentPhaseId: 'p_1',
-    clipboardMeals: null, // 식단 세트 복사 저장소
-    editingMealState: null, // 복제 및 편집 모달 제어용 상태
-    customSupps: JSON.parse(JSON.stringify(INITIAL_CUSTOM_SUPPS)),
-    foodDB: JSON.parse(JSON.stringify(FOOD_DB)),
-    foodCategories: JSON.parse(JSON.stringify(FOOD_CATEGORIES)),
-    pieChartInstance: null,
-    userId: null,
-    db: null,
-    appId: typeof __app_id !== 'undefined' ? __app_id : 'prep-master-pro'
+window.PrepStore = {
+    state: {
+        userInfo: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.INITIAL_USER_INFO)),
+        phases: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.INITIAL_PHASES)),
+        currentPhaseId: 'p_1',
+        clipboardMeals: null,
+        editingMealState: null,
+        customSupps: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.INITIAL_CUSTOM_SUPPS)),
+        foodDB: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.FOOD_DB)),
+        foodCategories: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.FOOD_CATEGORIES)),
+        pieChartInstance: null,
+        userId: null,
+        db: null,
+        appId: 'prep-master-pro'
+    },
+    
+    applyCustomSuppsToDB: function() {
+        this.state.foodCategories['보충제'] = [];
+        this.state.customSupps.forEach(supp => {
+            if(supp.weight > 0) {
+                this.state.foodDB[supp.name] = { 
+                    c: supp.carbs / supp.weight, 
+                    p: supp.protein / supp.weight, 
+                    f: supp.fat / supp.weight, 
+                    k: supp.kcal / supp.weight 
+                };
+                this.state.foodCategories['보충제'].push(supp.name);
+            }
+        });
+    }
 };
-
-export function applyCustomSuppsToDB() {
-    state.foodCategories['보충제'] = [];
-    state.customSupps.forEach(supp => {
-        if(supp.weight > 0) {
-            state.foodDB[supp.name] = { c: supp.carbs / supp.weight, p: supp.protein / supp.weight, f: supp.fat / supp.weight, k: supp.kcal / supp.weight };
-            state.foodCategories['보충제'].push(supp.name);
-        }
-    });
-}
