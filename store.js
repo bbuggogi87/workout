a3611c05f7baa7b4c7d7bf6d7d0c8620a6bc8ffd
bbuggogi87 <bbuggogi@gmail.com>
2026-06-27 04:15:36 +0900

    Refactor store.js to use ES6 module syntax

--- store.js
+++ store.js
@@ -1,31 +1,32 @@
-window.PrepStore = {
-    state: {
-        userInfo: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.INITIAL_USER_INFO)),
-        phases: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.INITIAL_PHASES)),
-        currentPhaseId: 'p_1',
-        clipboardMeals: null,
-        editingMealState: null,
-        customSupps: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.INITIAL_CUSTOM_SUPPS)),
-        foodDB: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.FOOD_DB)),
-        foodCategories: JSON.parse(JSON.stringify(window.PREP_CONSTANTS.FOOD_CATEGORIES)),
-        pieChartInstance: null,
-        userId: null,
-        db: null,
-        appId: 'prep-master-pro'
-    },
-    
-    applyCustomSuppsToDB: function() {
-        this.state.foodCategories['보충제'] = [];
-        this.state.customSupps.forEach(supp => {
-            if(supp.weight > 0) {
-                this.state.foodDB[supp.name] = { 
-                    c: supp.carbs / supp.weight, 
-                    p: supp.protein / supp.weight, 
-                    f: supp.fat / supp.weight, 
-                    k: supp.kcal / supp.weight 
-                };
-                this.state.foodCategories['보충제'].push(supp.name);
-            }
-        });
-    }
+import { FOOD_DB, FOOD_CATEGORIES, INITIAL_USER_INFO, INITIAL_CUSTOM_SUPPS, INITIAL_PHASES } from './constants.js';
+
+export const state = {
+    userInfo: JSON.parse(JSON.stringify(INITIAL_USER_INFO)),
+    phases: JSON.parse(JSON.stringify(INITIAL_PHASES)),
+    currentPhaseId: 'p_1',
+    clipboardMeals: null,
+    editingMealState: null,
+    editingPhaseIsNew: false,
+    customSupps: JSON.parse(JSON.stringify(INITIAL_CUSTOM_SUPPS)),
+    foodDB: JSON.parse(JSON.stringify(FOOD_DB)),
+    foodCategories: JSON.parse(JSON.stringify(FOOD_CATEGORIES)),
+    pieChartInstance: null,
+    userId: null,
+    db: null,
+    appId: 'prep-master-pro'
 };
+
+export function applyCustomSuppsToDB() {
+    state.foodCategories['보충제'] = [];
+    state.customSupps.forEach(supp => {
+        if(supp.weight > 0) {
+            state.foodDB[supp.name] = { 
+                c: supp.carbs / supp.weight, 
+                p: supp.protein / supp.weight, 
+                f: supp.fat / supp.weight, 
+                k: supp.kcal / supp.weight 
+            };
+            state.foodCategories['보충제'].push(supp.name);
+        }
+    });
+}
